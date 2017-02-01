310750296f5d9efa6d5fae04b364b68a462fee21
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 12:24:54 +0900

    Pass empty user_name & password to render

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -5,6 +5,8 @@ class AccountController extends Controller
 	public function signupAction()
 	{
 		return $this->render(array(
+			'user_name' => '',
+			'password' => '',
 			'_token' => $this->generateCsrfToken('account/signup'),
 		));
 	}
