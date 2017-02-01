611edd51544c89918cef163f951cdfc958fbc366
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 12:44:44 +0900

    Add status controller

--- MiniBlogApplication.php
+++ MiniBlogApplication.php
@@ -12,6 +12,8 @@ class MiniBlogApplication extends Application
 	protected function registerRoutes()
 	{
 		return array(
+			'/' => array('controller' => 'status','action' => 'index'),
+			'/status/post' => array('controller' => 'status','action' => 'post'),
 			'account' => array('controller' => 'account','action' => 'index'),
 			'account/:action' => array('controller' => 'account'),
 		);
