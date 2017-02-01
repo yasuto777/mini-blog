80df476ae9c322809ea76b97d450fd856d9dc27a
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 14:43:08 +0900

    Fixed syntax error

--- views/status/status.php
+++ views/status/status.php
@@ -1,11 +1,9 @@
-	<div class="status">
-		<div class ="status_content">
-			<?php echo $this->escape($status['user_name']); ?>
-			<?php echo $this->escape($status['body']); ?>
-		</div>
-		<div>
-			<?php echo $this->escape($status['created_at']); ?>
-		</div>
+<div class="status">
+	<div class ="status_content">
+		<?php echo $this->escape($status['user_name']); ?>
+		<?php echo $this->escape($status['body']); ?>
 	</div>
-	<?php endforeach; ?>
+	<div>
+		<?php echo $this->escape($status['created_at']); ?>
+	</div>
 </div>
