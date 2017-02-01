ea1755214751dc300d0560ee988478e2d39f869e
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 11:01:17 +0900

    Add layout file

---
+++ views/layout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3c.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+	<title><?php if (isset($title)): echo $this->escape($title) . ' - ';
+	endif; ?>Mini Blog</title>
+</head>
+<body>
+
+	<div id="header">
+		<h1><a href="<?php echo $base_url; ?>/">Mini Blog</a></h1>
+	</div>
+
+	<div id="main">
+		<?php echo $_content; ?>
+	</div>
+
+</body>
+</html>
