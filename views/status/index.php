51e46ef1bdcc69184ad30ea14d36b1d92943972d
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 14:16:37 +0900

    Split status class and call status.php

--- views/status/index.php
+++ views/status/index.php
@@ -19,14 +19,6 @@
 
 <div id="statuses">
 	<?php foreach ($statuses as $status): ?>
-	<div class="status">
-		<div class ="status_content">
-			<?php echo $this->escape($status['user_name']); ?>
-			<?php echo $this->escape($status['body']); ?>
-		</div>
-		<div>
-			<?php echo $this->escape($status['created_at']); ?>
-		</div>
-	</div>
+	<?php echo $this->render('status/status',array('status' => $status)); ?>
 	<?php endforeach; ?>
 </div>
---
+++ views/status/status.php
@@ -0,0 +1,11 @@
+	<div class="status">
+		<div class ="status_content">
+			<?php echo $this->escape($status['user_name']); ?>
+			<?php echo $this->escape($status['body']); ?>
+		</div>
+		<div>
+			<?php echo $this->escape($status['created_at']); ?>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</div>
