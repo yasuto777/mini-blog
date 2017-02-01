51930be39850a2d5f2868fa3cc2750e307d10d9e
yasuto777 <basilisk.0331@icloud.com>
2017-02-01 13:10:15 +0900

    Split signup file

--- views/account/signup.php
+++ views/account/signup.php
@@ -6,11 +6,6 @@
 	<input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
 
 <?php if (isset($errors) && count($errors) > 0): ?>
-<ul class="error_list">
-	<?php foreach ($errors as $error): ?>
-	<li><?php echo $this->escape($error); ?></li>
-	<?php endforeach; ?>
-</ul>
 <?php endif; ?>
 
 	<table>
