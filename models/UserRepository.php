0b04e7f700b46ce614a035eabdb21453dd6aef3d
yasuto777 <basilisk.0331@icloud.com>
2017-02-03 11:25:04 +0900

    Fixed fetchByUserName()

--- models/UserRepository.php
+++ models/UserRepository.php
@@ -25,11 +25,9 @@ class UserRepository extends DbRepository
 
 	public function fetchByUserName($user_name)
 	{
-		$sql = "SELECT COUNT(id) as count FROM user WHERE user_name = :user_name";
+		$sql = "SELECT * FROM user WHERE user_name = :user_name";
 
-		$row = $this->fetch($sql,array(':user_name' => $user_name));
-		if ($row['count'] === '0'){
-			return true;
+		return $this->fetch($sql,array(':user_name' => $user_name));
 		}
 
 		return false;
