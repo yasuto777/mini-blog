750b530498bb8570ba3d15d431357c7856c8b9f8
yasuto777 <basilisk.0331@icloud.com>
2017-02-06 10:50:46 +0900

    Display follow button

--- views/status/user.php
+++ views/status/user.php
@@ -2,6 +2,20 @@
 
 <h2><?php echo $this->escape($user['user_name']); ?></h2>
 
+<?php if (!is_null($following)): ?>
+<?php if ($following): ?>
+<p>フォローしています</p>
+<?php else: ?>
+<form action="<?php echo $base_url; ?>/follow" method="post">
+	<input type="hidden" name="_token" value="<?php echo $this->escape($_toekn); ?>" />
+	<input type="hidden" name="following_name" value="<?php echo 
+	$this->escape($user['user_name'])?>" />
+
+	<input type="submit" value="フォr-する" />
+</form>
+<?php endif; ?>
+<?php endif; ?>
+
 <div id="statuses">
 	<?php foreach ($statuses as $status): ?>
 	<?php echo $this->render('status/status',array('status' => $status)); ?>
