46b70b7776eb9f2322c0eb0d2fefc2f62b17be7a
yasuto777 <basilisk.0331@icloud.com>
2017-02-08 17:04:13 +0900

    New create update view file

---
+++ views/account/update.php
@@ -0,0 +1,34 @@
+<?php $this->setLayoutVar('title','パスワードの変更'); ?>
+
+<h2>パスワードの変更</h2>
+
+	<form action="<?php echo $base_url; ?>/account/changepass" method="post">
+		<input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
+
+	<table>
+		<tbody>
+			<tr>
+				<th>Password</th>
+				<td>
+					<input type="password" name="password" value="" />
+				</td>
+			</tr>
+			<tr>
+				<th>New Password</th>
+				<td>
+				<input type="password" name="new_password" value="" />
+				</td>
+			</tr>
+			<tr>
+				<th>New Password (again)</th>
+				<td>
+					<input type="password" name="check_new_password" value="" />
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+<p>
+	<input type="submit" value="Update" />
+</p>
+</form>
