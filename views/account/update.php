79d7ef2e2363b9c1daa8241b4c9743a50922bbfc
souma <souma@headjapan.com>
2017-02-08 18:10:31 +0900

    Display user_name

--- views/account/update.php
+++ views/account/update.php
@@ -1,6 +1,7 @@
 <?php $this->setLayoutVar('title','パスワードの変更'); ?>
 
 <h2>パスワードの変更</h2>
+	ユーザーID：<?php echo $this->session->getPost['user_name']; ?>
 
 	<form action="<?php echo $base_url; ?>/account/changepass" method="post">
 		<input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
