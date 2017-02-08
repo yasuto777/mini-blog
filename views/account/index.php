d109260de94ac95908529d1781ce0a8c440bd8bc
souma <souma@headjapan.com>
2017-02-08 18:23:51 +0900

    Create link for update

--- views/account/index.php
+++ views/account/index.php
@@ -16,6 +16,9 @@
 	<li>
 		<a href="<?php echo $base_url; ?>/account/signout">ログアウト</a>
 	</li>
+	<li>
+		<a href="<?php echo $base_url; ?>/account/update">パスワードの変更</a>
+	</li>
 </ul>
 
 <h3>フォロー中</h3>
