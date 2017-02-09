577f8c72668a4d70c0252b80133f4804dcb4fa99
souma <souma@headjapan.com>
2017-02-09 12:36:18 +0900

    Replace tab to space

--- views/status/index.php
+++ views/status/index.php
@@ -3,21 +3,26 @@
 <h2>ホーム</h2>
 
 <form action="<?php echo $base_url; ?>/status/post" method="post">
-	<input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
+    <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
 
-	<?php if (isset($errors) && count($errors) > 0): ?>
-	<?php echo $this->render('errors',array('errors' => $errors)); ?>
-	<?php endif; ?>
+    <?php if (isset($errors) && count($errors) > 0): ?>
+    <?php echo $this->render('errors',array('errors' => $errors)); ?>
+    <?php endif; ?>
 
-	<textarea name="body" rows="2" cols="60"><?php echo $this->escape($body); ?></textarea>
+    <textarea name="body" rows="2" cols="60"><?php echo $this->escape($body); ?></textarea>
 
-	<p>
-		<input type="submit" value="発言" />
-	</p>
+    <p>
+        <input type="submit" value="発言" />
+    </p>
 </form>
 
+<!-- Author souma -->
+<ul style="list-style:none">
+    <li><a href="">すべての発言を表示</a></li>
+</ul>
+
 <div id="statuses">
-	<?php foreach ($statuses as $status): ?>
-	<?php echo $this->render('status/status',array('status' => $status)); ?>
-	<?php endforeach; ?>
+    <?php foreach ($statuses as $status): ?>
+    <?php echo $this->render('status/status',array('status' => $status)); ?>
+    <?php endforeach; ?>
 </div>
